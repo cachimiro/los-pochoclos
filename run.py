d8aa0e791a2539f6ccd19b543df21d6066bf6047
Ubuntu <ubuntu@ip-172-31-94-249.ec2.internal>
2019-12-21 18:14:52 +0000

    add email.js and debuged the rooms. html

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 import os
 import json
 import click
+from flask_mail import Message
 from flask import Flask, render_template, request,redirect, flash, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+
 
 
 app = Flask(__name__)
@@ -63,13 +65,9 @@ def about_member(member_name):
     
 
 
-@app.route('/contact', methods=["GET", "POST"])
-def contact():
-    if request.method == "POST":
-        flash("Thanks {}, we have received your message".format(
-            request.form["name"]
-        ))
-    return render_template("contact.html", page_title="Contact")
+@app.route('/contact')
+def contact():  
+ return render_template("contact.html", page_title="Contact",)
 
 
 @app.route('/rooms')
@@ -118,53 +116,3 @@ if __name__ == '__main__':
             
    
          
-# chat function will go here all the code thats going to be provided from here is for that chat area
-#pesonal note i will coment out this section for a moment need to work on other html 
-"""
-import os
-from datetime import datetime
-from flask import Flask, redirect, render_template, request, session, url_for
-
-
-app = Flask(__name__)
-app.secret_key = os.getenv("SECRET","randomstring123")
-messages = []
-
-
-def add_message(username, message):
-    #Add messages to the `messages` list
-    now = datetime.now().strftime("%H:%M:%S")
-    messages.append({"timestamp": now, "from": username, "message": message})
-
-
-@app.route("/", methods=["GET", "POST"])
-def chat_room():
-    #Main page with instructions
-    if request.method == "POST":
-        session["username"] = request.form["username"]
-
-    if "username" in session:
-        return redirect(url_for("user", username=session["username"]))
-
-    return render_template("index.html")
-
-
-@app.route("/chat/<username>", methods=["GET", "POST"])
-def user(username):
-    #Add and display chat messages
-    if request.method == "POST":
-        username = session["username"]
-        message = request.form["message"]
-        add_message(username, message)
-        return redirect(url_for("user", username=session["username"]))
-
-    return render_template("chat.html", username=username,
-                           chat_messages=messages)
-
-
-app.run(host=os.getenv("IP", "0.0.0.0"),
-        port=int(os.getenv("PORT", "5000")), debug=False)
-       
- """
- 
- 
